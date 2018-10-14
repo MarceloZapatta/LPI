93a2c2994a136833c07a9b3a683d0d69a233d43b
Marcelo Zapatta <zaps@zapson.localdomain>
2018-10-14 16:55:32 -0300

    Correcoes de bugs exercicio 1

---
+++ Lista2/1/1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    // String para armazenar os dígitos ISBN
+    char isbn[11];
+    
+    while (strcmp(isbn, "FIM") != 0) {
+
+        /* code */
+        scanf("%s", &isbn);
+        if(strcmp(isbn, "FIM") == 0) {
+            return 0;
+        }
+
+        // Acumuladores
+        int s1[10] = {0}, s2 = 0;
+        int k = 0, j = 0, resto;
+        
+        for(int i = 0; i < 11; i++) {
+            if(i == 0) {
+                *s1 = *(isbn + i) - '0';
+                k++;
+            } else if(i != 9) {
+                *(s1 + k) += *(s1 + (k - 1)) + (*(isbn + i) - '0');
+                k++;
+            } else if(i == 10) {
+                if(*(isbn + i) == 'X') {
+                    *(s1 + k) += (*(s1 + (k - 1)) + 10);
+                } else {
+                    *(s1 + k) += *(s1 + (k - 1)) + (*(isbn + i) - '0');
+                }
+            }
+        }
+
+        for(int i = 0; i < 10; i++)
+        {
+            s2 += *(s1 + i);
+        }
+
+        resto = s2 % 11;
+
+        if(resto == 0) {
+            printf("ISBN %s e' va'lido\n", isbn);
+        } else {
+            printf("ISBN inva'lido: %i\n", resto);
+        }
+        
+    }
+}
