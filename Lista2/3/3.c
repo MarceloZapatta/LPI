2b0a4e3d85247159eb2bf058ae21b3dc785e25c5
Marcelo Andre Barreto Zapatta <marcelo@cgd.com.br>
2018-10-17 13:31:56 -0300

    Ideias e debug exercicio 3

--- Lista2/3/3.c
+++ Lista2/3/3.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     int posicao;
-    char str1[30], str2[30];
+    char * str1;
+    char * str2;
 
-    scanf("%s", &str1);
+    scanf("%[s\n]", str1);
+    printf("aaa%d\n", strlen(str1));
+    return 0;
     scanf("%s", &str2);
     scanf("%i", &posicao);
-
-
 
     return 0;
 }
 
-char strReplace(char * str1, char * str2, int posicao) {
+// char strReplace(char * str1, char * str2, int posicao) {
     
-    int i = 0, j = posicao - 1;
+//     int i = 0, j = posicao - 1;
     
-    while(*(str2 + i) != '\0'){
-        *(str1 + j) = *(str2 + i);
-        i++;
-        j++;
-    }
+//     while(*(str2 + i) != '\0'){
+//         *(str1 + j) = *(str2 + i);
+//         i++;
+//         j++;
+//     }
     
-    return str1;
-}
+//     return str1;
+// }
