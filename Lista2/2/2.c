9ef6138d665216055c35e08a76c3417b2c4849c4
Marcelo Andre Barreto Zapatta <marcelo@cgd.com.br>
2018-10-17 13:24:58 -0300

    Ideias Rotina Cripto

--- Lista2/2/2.c
+++ Lista2/2/2.c
@@ -2,23 +2,25 @@
 
 int main()
 {
-    
+    char texto1[9], texto2[9], texto3[9];
+    printf( "Informe um texto de até 8 caracteres:" ) ;
+    scanf( "%[^\n]", texto1 );
+    Cripto(texto1, texto2);
+    Cripto(texto2, texto3);
+    printf("O texto criptografado eh: \n%s\n e descriptografado eh: \n%s\n", texto2, texto3);
+
     return 0;
 }
 
-char * Cripto(char * str1, char * str2) {
-    
-    for(int i = 0; i < 9; i++)
-    {
-        // Pesquisa na segunda string o caracter
+void Cripto(char * str1, char * str2) {
+    char cadeia[52] = 'abcdefghijkl...';
+
+    for(int i = 0; i < 52; i++) {
         for(int j = 0; j < 9; j++)
         {
-            if(*(str1 + i) == *(str2 + j)) {
+            if(*(str1 + j) == *(cadeia + i)) {
                 
             }
         }
-        
     }
-    
-    return *str2;
 }
