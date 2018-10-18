cb2bc8f8506fbfc73228bb5d4c11d581d16e11eb
Marcelo Andre Barreto Zapatta <marcelo@cgd.com.br>
2018-10-18 13:30:43 -0300

    Exercicio 3 Lista

--- Lista2/3/3.c
+++ Lista2/3/3.c
@@ -2,28 +2,35 @@
 #include <string.h>
 
 int main() {
-    int posicao;
-    char * str1;
-    char * str2;
+    int posicao, controle = 0;
+    char str1[20];
+    char str2[20];
 
-    scanf("%[s\n]", str1);
-    printf("aaa%d\n", strlen(str1));
-    return 0;
-    scanf("%s", &str2);
-    scanf("%i", &posicao);
+    printf("Indique a primeira string(ate 10 caracteres):\n");
+    scanf("%s", str1);
+
+    while(controle == 0) {
+        printf("Indique a segunda string(ate 10 caracteres):\n");
+        scanf("%s", str2);
+
+        printf("Indique a posicao para trocar:\n");
+        scanf("%i", &posicao);
+    }
+
+    str_replace(str1, str2, posicao);
+
+    printf("A nova string: \"%s\"", str1);
 
     return 0;
 }
 
-// char strReplace(char * str1, char * str2, int posicao) {
+void str_replace(char * str1, char * str2, int posicao) {
     
-//     int i = 0, j = posicao - 1;
+    int i = 0, j = posicao - 1;
     
-//     while(*(str2 + i) != '\0'){
-//         *(str1 + j) = *(str2 + i);
-//         i++;
-//         j++;
-//     }
-    
-//     return str1;
-// }
+    while(*(str2 + i) != '\0'){
+        *(str1 + j) = *(str2 + i);
+        i++;
+        j++;
+    }
+}
