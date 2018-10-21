77409f26fb044dee1dde6cfa9dea9251fa4f4483
MarceloZapatta <MarceloZapatta>
2018-10-21 11:55:58 -0300

    Correções de bugs ex 3

--- Lista2/3/3.c
+++ Lista2/3/3.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+void str_replace(char * str1, char * str2, int posicao);
+
 int main() {
-    int posicao = 0;
+    int posicao = 0, stop = 0;
     char str1[20];
     char str2[20];
 
-    do {
+    while (!stop) {
         printf("Indique a primeira string(ate 20 caracteres):\n");
         scanf("%s", str1);
 
@@ -15,13 +17,14 @@ int main() {
 
         printf("Indique a posicao para trocar:\n");
         scanf("%i", &posicao);
+        if(posicao < 0) {
+            return 0;
+        }
 
         str_replace(str1, str2, posicao);
 
         printf("A nova string: \"%s\"\n", str1);
-    } while(posicao > 0);
-
-    return 0;
+    }
 }
 
 void str_replace(char * str1, char * str2, int posicao) {
