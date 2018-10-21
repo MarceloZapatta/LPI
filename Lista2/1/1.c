7a46a706be45a186af39c06967874481998fffea
MarceloZapatta <MarceloZapatta>
2018-10-21 12:23:31 -0300

    Comentarios no codigo e correção de bug na cadeia de controle

--- Lista2/1/1.c
+++ Lista2/1/1.c
@@ -21,17 +21,28 @@ int main()
         int s1[10] = {0}, s2 = 0;
         int k = 0, j = 0, resto;
         
+        // Percorre o ISBN
         for(int i = 0; i < 11; i++) {
+
+            // Primeiro caracter
             if(i == 0) {
                 *s1 = *(isbn + i) - '0';
                 k++;
             } else if(i != 9 && i != 10) {
+
+                // Pega o digito com excecao das 2 ultimas casas
                 *(s1 + k) += *(s1 + (k - 1)) + (*(isbn + i) - '0');
                 k++;
             } else if(i == 10) {
+
+                // Caso seja a ultima casa verifica se é X
                 if(*(isbn + i) == 'X') {
+
+                    // true soma 10 a ultima casa
                     *(s1 + k) += (*(s1 + (k - 1)) + 10);
                 } else {
+
+                    // se não soma com o respectivo número
                     *(s1 + k) += *(s1 + (k - 1)) + (*(isbn + i) - '0');
                 }
             }
@@ -39,11 +50,13 @@ int main()
 
         for(int i = 0; i < 10; i++)
         {
+            // Preenche o segundo acumulador
             s2 += *(s1 + i);
         }
 
         resto = s2 % 11;
 
+        // Valida o ISBN
         if(resto == 0) {
             printf("ISBN %s e' va'lido\n", isbn);
         } else {
--- Lista2/2/2.c
+++ Lista2/2/2.c
@@ -8,27 +8,46 @@ int main()
     char texto1[9], texto2[9], texto3[9];
     printf( "Informe um texto de até 8 caracteres:\n" ) ;
     scanf( "%[^\n]", texto1 );
+
     Cripto(texto1, texto2);
     Cripto(texto2, texto3);
+
     printf("O texto criptografado eh: \n%s\n e descriptografado eh: \n%s\n", texto2, texto3);
 
     return 0;
 }
 
+/**
+ * Função para criptografar/descripritografar um número
+ * @param char * str 1 Recebe a string a ser (des)criptografa
+ * @param char * str 2 String onde sera salvo a nova string.
+ */
 int Cripto(char * str1, char * str2) {
+    
+    // Strings de controle para criptografia
     char controle1[19] = "afhsu]i@lD5,23&eyPc";
     char controle2[19] = "gFp04#:w^ld9!/*?.;z";
     int i, j;
 
+    // Percorre a str1 entrada
     for(i = 0; i < 9; i++) {
-        for(j = 0; j < 15; j++) {
+
+        // Percorre as cadeias de controle
+        for(j = 0; j < 19; j++) {
+
+            // Se o caracter estiver presente na primeira
+            // cadadeia de controle
             if(*(str1 + i) == *(controle1 + j)) {
                 *(str2 + i) = *(controle2 + j);
                 break;
             } else if(*(str1 + i) == *(controle2 + j)) {
+
+                // Presente na segunda cadeia
                 *(str2 + i) = *(controle1 + j);
                 break;
             } else {
+
+                // permanece o mesmo
                 *(str2 + i) = *(str1 + i);
             }
         }
